7800f52d628ef761a1911b8a41053e00be3faf42
Rushiraj11 <grushiraj95@gmail.com>
2021-09-01 20:37:49 +0530

    Added: Length of Line solution using Cartesion Co-ordinates

--- src/main/java/com/linecomaprisonOOPs/LineComparison.java
+++ src/main/java/com/linecomaprisonOOPs/LineComparison.java
@@ -1,13 +1,30 @@
 package com.linecomaprisonOOPs;
-
+import java.util.Scanner;
 public class LineComparison {
-
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         LineComparison lineMethod = new LineComparison();//created object to call method welcomemessage as it is not static
         lineMethod.welcomemessage();
+        lineMethod.lengthOfLine();
     }
      private void welcomemessage(){
          System.out.println("Welcome to Line Comparison");
 
      }
+    public void lengthOfLine(){
+        System.out.println("Enter Cartesian Co-ordinates");
+        System.out.println("Enter x1 ");
+        int x1 = sc.nextInt();
+        System.out.println("Enter x2 ");
+        int x2 = sc.nextInt();
+        System.out.println("Enter y1 ");
+        int y1 = sc.nextInt();
+        System.out.println("Enter y2 ");
+        int y2 = sc.nextInt();
+        System.out.println("Length of the line:"+(Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1),2)));
+    }
 }
