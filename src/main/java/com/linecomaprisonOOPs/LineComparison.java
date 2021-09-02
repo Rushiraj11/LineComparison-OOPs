27bfb13762ef6583cc79aa445a5361e9b21856fb
Rushiraj11 <grushiraj95@gmail.com>
2021-09-02 22:56:42 +0530

    Added: Equality and Compare to Method

--- src/main/java/com/linecomaprisonOOPs/LineComparison.java
+++ src/main/java/com/linecomaprisonOOPs/LineComparison.java
@@ -1,7 +1,7 @@
 package com.linecomaprisonOOPs;
 import java.util.Scanner;
 public class LineComparison {
-    int x1, x2, x3, x4; // instance variables
+    int x1, x2, x3, x4;// instance variables
     int y1, y2, y3, y4;
     double line1;
     double line2;
@@ -9,15 +9,15 @@ public class LineComparison {
 
 
     public static void main(String[] args) {
-        LineComparison lineMethod = new LineComparison();//created object to call method welcomemessage as it is not static
-        lineMethod.welcomemessage();
-        lineMethod.compareLines();
 
+        LineComparison lineMethod = new LineComparison();
+       lineMethod.welcomemessage();//created object to call method welcomemessage as it is not static
+        lineMethod.equalsTo();
+        lineMethod.compareLines();
     }
 
-    private void welcomemessage() {
+    public void welcomemessage() {
         System.out.println("Welcome to Line Comparison");
-
     }
 
     public double lengthOfLine1() {
@@ -31,10 +31,9 @@ public class LineComparison {
         System.out.println("Enter y2 ");
         y2 = sc.nextInt();
         line1 = Math.sqrt(Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2);
-        System.out.println("Length of the line: " + line1);
-          return line1;
+        System.out.println("Length of the line1: " + line1);
+        return line1;
     }
-
 
     public double lengthOfLine2() {
         System.out.println("Enter Cartesian Co-ordinates");
@@ -46,21 +45,27 @@ public class LineComparison {
         y3 = sc.nextInt();
         System.out.println("Enter y4 ");
         y4 = sc.nextInt();
-        line2 = Math.sqrt(Math.pow((x4 - x3), 2)) + Math.pow((y4 - y3), 2);
-        System.out.println("Length of the line: " + line1);
+        line2 = Math.sqrt(Math.pow((x4 - x3),2)) + Math.pow((y4 - y3),2);
+        System.out.println("Length of the line2: " + line2);
         return line2;
     }
 
-    public void compareLines() {
-        double line1Length = lengthOfLine1();
-        double line2Length = lengthOfLine2();
+    double line1Length = lengthOfLine1();
+    double line2Length = lengthOfLine2();
+    public void equalsTo() {
         if (line1Length == line2Length) {
             System.out.println("Lines are Equal");
-        } else if (line1Length > line2Length) {
-            System.out.println("Line1 is greater than Line2 ");
-        } else{
-            System.out.println("lines2 is greater than line1");
+        } else {
+            System.out.println("Call compareLines method");
         }
     }
 
-}
+    public void compareLines() {
+            if (line1Length > line2Length) {
+                System.out.println("Line1 is greater than Line2 ");
+            } else {
+                System.out.println("lines2 is greater than line1");
+            }
+        }
+    }
+
